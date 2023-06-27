6c3d10af3d73e403430bd53efb634d3a0a8d1215
Chris Markiewicz <effigies@gmail.com>
2023-06-27 09:59:49 -0400

    TEST: Use specific failing list

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 import numpy as np
 
 def test_reproduction():
-    assert np.array_equal(np.argsort([1, 0, 1]), [1, 0, 2])
+    assert np.array_equal(np.argsort([1000] * 6 + [0, 1000]), [6, 0, 1, 2, 3, 4, 5, 7])
