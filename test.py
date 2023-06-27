6be0abeb4cb33cb7515c48327192d613335f6ac6
Chris Markiewicz <effigies@gmail.com>
2023-06-27 10:09:40 -0400

    Update test to be callable directly with Python

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 import numpy as np
+import pytest
 
 def test_reproduction():
     assert np.array_equal(np.argsort([1000] * 6 + [0, 1000]), [6, 0, 1, 2, 3, 4, 5, 7])
+
+if __name__ == '__main__':
+    pytest.main(['-sv', __file__])
